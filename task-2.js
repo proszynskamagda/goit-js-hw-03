782ccdeb096eb38d446554c0fec3c6728a5b95e7
MagdaP <proszynskamagda@gmail.com>
2024-10-02 20:27:31 +0200

    jndlsm

---
+++ task-2.js
@@ -0,0 +1,6 @@
+function makeArray(firstArray, secondArray, maxLength) {
+  const newArray = [];
+  firstPartOfNewArray = firstArray.push(secondArray);
+  newArray = firstPartOfNewArray.push(maxLength);
+  return newArray;
+}
